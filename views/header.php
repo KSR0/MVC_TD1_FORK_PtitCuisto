4831644ae13af1da5e4b3757c4f2dc5e9857f5bf
Bayon Axel <axel.bayon14@gmail.com>
2023-11-08 00:46:13 +0100

    Updating to a newer version

--- views/header.php
+++ views/header.php
@@ -74,19 +74,25 @@
 
                 <div class="leading-7 text-left text-sm font-thin mt-2 w-4/5 mx-auto" id="submenu">
 
-                    <div onclick="desactiver_sous_menu(); desactiver_sous_menu_deroulant(); desactiver_bouton_mobile(); desactiver_logo_menu(); fond_clair(); fond_clair_deroulant(); fond_clair_logo()" id="categorie" class="element_menu cursor-pointer modal-open1 p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 hover:bg-charte_bleu_fonce">
+                    <hr class="my-2 mt-3 text-charte_blanc">
+
+                    <div id="categorie" class="cursor-pointer modal-open1 p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 hover:bg-charte_bleu_fonce">
                         <i class="bi bi-book"></i>
                         <p data-modal-target="staticModal" data-modal-toggle="staticModal" class="text-[15px] ml-4 text-gray-200">Catégorie</p>
                         <script>ouvrir_modale(1)</script>
                     </div>
 
-                    <div onclick="desactiver_sous_menu(); desactiver_sous_menu_deroulant(); desactiver_bouton_mobile(); desactiver_logo_menu(); fond_clair(); fond_clair_deroulant(); fond_clair_logo()" id="titre" class="element_menu cursor-pointer modal-open2 p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 hover:bg-charte_bleu_fonce">
+                    <hr class="my-2 mt-3 text-charte_blanc">
+
+                    <div id="titre" class="cursor-pointer modal-open2 p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 hover:bg-charte_bleu_fonce">
                         <i class="bi bi-search"></i>
                         <p data-modal-target="staticModal" data-modal-toggle="staticModal" class="text-[15px] ml-4 text-gray-200">Titre</p>
                         <script>ouvrir_modale(2)</script>
                     </div>
 
-                    <div onclick="desactiver_sous_menu(); desactiver_sous_menu_deroulant(); desactiver_bouton_mobile(); desactiver_logo_menu(); fond_clair(); fond_clair_deroulant(); fond_clair_logo()" id="ingredient" class="element_menu cursor-pointer modal-open3 p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 hover:bg-charte_bleu_fonce">
+                    <hr class="my-2 mt-3 text-charte_blanc">
+
+                    <div id="ingredient" class="cursor-pointer modal-open3 p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 hover:bg-charte_bleu_fonce">
                         <i class="bi bi-cart"></i>
                         <p data-modal-target="staticModal" data-modal-toggle="staticModal" class="text-[15px] ml-4 text-gray-200">Ingrédient(s)</p>
                         <script>ouvrir_modale(3)</script>
--- views/modale_titre.php
+++ views/modale_titre.php
@@ -1,4 +1,4 @@
-<div id="staticModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="lg:pl-80 modal2 opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
+<div id="staticModal" data-modal-backdrop="static" aria-hidden="true" class="z-20 modal2 opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
     <div id="modal2-test" class="modal2-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>                                
     <div class="modal2-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
         
@@ -8,7 +8,7 @@
                 <p class="text-2xl font-bold">Filtre par titre</p>
                 
                 <div class="modal2-close cursor-pointer z-50">
-                    <button onclick="activer_sous_menu(); activer_sous_menu_deroulant(); activer_bouton_mobile(); activer_logo_menu(); fond_fonce(); fond_fonce_deroulant()">
+                    <button>
                         <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
                             <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
                         </svg>
@@ -17,7 +17,7 @@
             </div>
 
             
-            <form>
+            <form action="filtre_titre.php" method="post">
                 <div class="relative">
 
                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -26,8 +26,8 @@
                         </svg>
                     </div>
 
-                    <input type="search" id="search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Saisissiez un titre" required>
-                    <button type="submit" onclick="bouton_recherche_par_titre()" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Rechercher</button>
+                    <input type="search" id="search" name="titre" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Saisissiez un titre" required>
+                    <button type="submit" name="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Rechercher</button>
                 </div>
             </form>
         </div>
