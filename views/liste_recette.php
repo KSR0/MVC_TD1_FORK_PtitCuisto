0ea15dd558efb9c1748d46b2fa08c83171e8cd94
Bayon Axel <axel.bayon14@gmail.com>
2023-11-08 13:37:39 +0100

    editing button

--- views/liste_recette.php
+++ views/liste_recette.php
@@ -11,12 +11,9 @@
         
 </div>
 
-
-<hr class='border-2 border-black'><br>
-
 <div class="mx-auto text-center">
     <button id="btnPlus" onclick="afficherRecettes()" class="text-charte_bleu_clair hover:text-charte_bleu_fonce border border-charte_bleu_fonce hover:bg-charte_bleu_clair focus:ring-4 focus:outline-none font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2">
-        Plus
+        Voir plus de recettes
     </button>
 </div>
 
